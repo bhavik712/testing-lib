68d9eb5b42c10b26e077d9c8db9924b0fa208ab2
bhavik712 <baradbhavik41@gmail.com>
2023-08-11 19:27:51 +0530

    tested library by giving local file path

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <form-input :fieldProperties="nameField"></form-input>
+  <form-input :fieldProperties="emailField"></form-input>
+</template>
+
+<script>
+import { FormInput } from "vue-component-lib";
+import "vue-component-lib/style.css";
+export default {
+  name: "app",
+  data() {
+    return {
+      emailField: {
+        label: "Email",
+        name: "userEmail",
+        type: "email",
+        placeholder: "Enter Your Email",
+      },
+      nameField: {
+        label: "Name",
+        name: "userName",
+        type: "text",
+        placeholder: "Enter Your Name",
+      },
+    };
+  },
+  components: {
+    FormInput,
+  },
+};
+</script>
